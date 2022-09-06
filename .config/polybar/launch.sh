a77209d7d6b37f54b219cedca48d4f52ec9b5be6
Jiei <109078709+Jiei-720p@users.noreply.github.com>
2022-09-06 12:01:37 +0800

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,14 +1,20 @@
-#!/bin/bash
-
-
-
+#! /usr/bin/env bash
 
 killall -q polybar
 
-
-while pgrep -x polybar >/dev/null; 
+while pgrep -u $UID -x polybar > /dev/null;
 do sleep 1; 
 done
 
+desktop=$(echo $DESKTOP_SESSION)
 
-polybar main &
+for M in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+				if [ $M == 'eDP-1' ]
+				then
+								MONITOR=$M polybar main &
+
+				elif [ $M == 'HDMI-1' ]
+				then
+								MONITOR=$M polybar main &
+				fi
+done
