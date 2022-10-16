fa8d15117d1b2cb5932df3e6405034f2aff9a1a9
Jiei <109078709+Jiei-720p@users.noreply.github.com>
2022-10-16 17:11:38 +0800

    Update battery_.sh

--- .config/polybar/scripts/battery_.sh
+++ .config/polybar/scripts/battery_.sh
@@ -1,16 +1,22 @@
-#! /usr/bin/bash
+#! /usr/bin/env bash
 
+# Getting the data and initializing an array.
 BATTERY_INFO=($( acpi | awk -F',' '{ print $0 }'))
+
+# Formatting helpers
 CHARGE=$((${BATTERY_INFO[3]//%,}))
 ICON=""
 FORMAT=""
 
+# Battery icon to reflect on the bar.
 if [[ "${BATTERY_INFO[2]}" == *"Charging"* ]] || [[ "${BATTERY_INFO[2]}" == *"Unknown"* ]] ; then
-    ICON="  "
+    ICON="  "
 else
     ICON="   "
 fi
 
+# Format charge & color depending on the status.
 FORMAT="$FORMAT $ICON $CHARGE%"
 
+# Display on bar
 echo $FORMAT
