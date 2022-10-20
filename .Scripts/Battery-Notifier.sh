af82baf68d9fdcfe1af49d82a2cf5647ad596bef
Jiei <109078709+Jiei-720p@users.noreply.github.com>
2022-10-20 12:56:28 +0800

    Update Battery-Notifier.sh
    Made the script check for old instances and kills that before executing... Also added a bunch of comments to make people's lives easier... probably...

--- .Scripts/Battery-Notifier.sh
+++ .Scripts/Battery-Notifier.sh
@@ -1,27 +1,43 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 
+# This part checks for old instance and kills it before executing
+for pid in $(pgrep -f $0);
+do
+	if [ $pid != $$ ]; 
+	then
+		kill $pid
+	fi
+done
+
+# This part checks your battery using acpi and sends you notification using dunst daemon
 while true
 do
-	
+
 	Bat_Pct=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 	Bat_Stat=$(acpi -b | grep -c "Charging")
-
+	
+	# This part checks whether your battery is at __% and tells you the battery is full
 	if [ "$Bat_Stat" -eq 1 ]; then	
 		if [ "$Bat_Pct" -ge 80 ]; then
 			dunstify -t 10000 -u normal "Battery full." "Level: ${Bat_Pct}% "
-			#play /
+			
+			# This part can play you a notification sound using sox
+			#play /path/to/file
 		fi
 
+	# This part checks whether your battery is at _% and tells you the battery is low
 	elif [ "$Bat_Stat" -eq 0 ]; then
-		if [ "$Bat_Pct" -le 20 ]; then
+		if [ "$Bat_Pct" -le 25 ]; then
 			dunstify -t 10000 -u critical "Low Battery." "Level: ${Bat_Pct}%" "Plug the Charger!"
-			#play /home/$USER/Music/Source/News-Intro.mp3
+			
+			# This part can play you a notification sound using sox
+			#play /path/to/file
 
-		elif [ "$Bat_Pct" -le 5 ]; then
+		# This part checks if your battery is at _% and suspends your session until you charge your laptop
+		elif [ "$Bat_Pct" -le 15 ]; then
 			dunstify -t 1000 -u critical "Battery at Critical Level." "Suspending..."
 			sleep 10
 			systemctl suspend
-
 		fi
 	fi
 	sleep 300
